59128614ca5a3e735976815db4bb8f7ba4fffd11
GRoblivium <lgreyesmiranda@gmail.com>
2025-01-20 18:16:37 -0300

    Código terminado

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo(){
+    const amigoImput = document.getElementById('amigo');
+    const nombre = amigoImput.value.trim();
+    if (nombre === '' || nombre.length === 0){
+        alert('El nombre del amigo es requerido');
+    }
+    else {
+        amigos.push(nombre);
+        actualizarLista();
+        amigoImput.value = '';
+    }    
+}
+function actualizarLista() {
+    const listaNombres = document.getElementById('listaAmigos');
+    listaAmigos.innerHTML = ''; // Limpiar la lista
+    amigos.forEach(nombre => {
+        const li = document.createElement('li');
+        li.textContent = nombre;
+        listaNombres.appendChild(li);
+    });
+}
+
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert('La lista está vacía. Por favor, agrega al menos un nombre.');
+    } else {
+        const indiceAleatorio = Math.floor(Math.random() * amigos.length);
+        const amigoSeleccionado = amigos[indiceAleatorio];
+        document.getElementById('resultado').textContent = `Amigo secreto seleccionado: ${amigoSeleccionado}`;
+    }
+}
+
